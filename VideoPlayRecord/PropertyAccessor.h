696f8dda3fac8b5f4c38fd1ca74780988cb7a279
Kodjo Baah <kodjo.baah@valtech.co.uk>
2013-10-07 13:03:16 +0100

    cleanup properties list access

---
+++ VideoPlayRecord/PropertyAccessor.h
@@ -0,0 +1,13 @@
+//
+//  PropertyAccessor.h
+//  VideoPlayRecord
+//
+//  Created by Valtech UK on 07/10/2013.
+//  Copyright (c) 2013 What am I doing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PropertyAccessor : NSObject
+
+@end
---
+++ VideoPlayRecord/PropertyAccessor.m
@@ -0,0 +1,13 @@
+//
+//  PropertyAccessor.m
+//  VideoPlayRecord
+//
+//  Created by Valtech UK on 07/10/2013.
+//  Copyright (c) 2013 What am I doing. All rights reserved.
+//
+
+#import "PropertyAccessor.h"
+
+@implementation PropertyAccessor
+
+@end
